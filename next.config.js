bff2d3f53451a11b42f19752af9d2353d203d185
Shasha12oly <147236264+Shasha12oly@users.noreply.github.com>
2026-03-26 17:58:17 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Basic configuration for production deployment
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = ['pg'];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
